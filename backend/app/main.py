758b8bff35d9a581aea77d0652d32be9fe8bc061
habib-dev92 <habib.khancw9@gmail.com>
2026-07-04 16:40:10 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-
+)
 app.include_router(auth_router, prefix="/api")
 app.include_router(router, prefix="/api")
 app.include_router(admin_router, prefix="/api")
